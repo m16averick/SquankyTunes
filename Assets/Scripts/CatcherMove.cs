fe6b329ccc6121b7cac683ab40e0003b0326b295
m16averick <298282@uwr.edu.pl>
2019-05-27 18:39:49 +0200

    Lerp camera
    Smooth following

---
+++ Assets/Scripts/CatcherMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CatcherMove : MonoBehaviour {
+
+	public Transform thePlayer;
+	private float PlayerPosX;
+
+	// Use this for initialization
+	void Start () {
+		PlayerPosX = thePlayer.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (thePlayer.position.x, transform.position.y, transform.position.z);		
+	}
+}
